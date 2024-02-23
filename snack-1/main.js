e973fcc8a2f20e9e70db70c206b995c47cbd5359
fraberret <f.berretta02@gmail.com>
2024-02-23 12:16:49 +0100

    snack 1 finish

---
+++ snack-1/main.js
@@ -0,0 +1,30 @@
+/*L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
+
+Tools
+-prompt
+-let/const
+-consolelog
+
+Steps
+1. chiedere il numero 2 volte
+2. salva in delle variabili
+3. if num 1>num2 log
+4. else log
+
+*/
+
+let firstNumber = prompt('Inserisci il numero')
+//console.log(firstNumber);
+
+let secondNumber = prompt('Inserisci il numero')
+
+if(firstNumber>secondNumber){
+    console.log(firstNumber);
+}else{
+    console.log(secondNumber);
+}
+
+
+
+
